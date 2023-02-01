d107770c17c1b96f565ea0875a7cbc92e8fb0cbe
Kevyn SOLA <kevyn.sola@gmail.com>
2023-02-01 09:58:50 +0100

    add target entity and form to kill

--- src/Controller/ChoiceProController.php
+++ src/Controller/ChoiceProController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Killer;
+use App\Entity\Target;
+use App\Form\TargetType;
 use App\Repository\KillerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,14 +22,36 @@ class ChoiceProController extends AbstractController
         ]);
     }
 
-    #[Route('/choice/{killer}', methods: ['GET'], name: 'app_choice_killer')]
+    #[Route('/choice/{killer}', methods: ['GET', 'POST'], name: 'app_choice_killer')]
     public function choiceKiller(
+        Request $request,
         KillerRepository $killerRepository,
-        Killer $killer
+        Killer $killer,
+        EntityManagerInterface $entityManager
     ): Response
     {
+        $target = new Target();
+        $form = $this->createForm(TargetType::class, $target);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $killer; // set kill target.
+
+            $entityManager->persist($target);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_valid_order');
+        }
+
         return $this->render('choiceUser/targetKill.html.twig', [
             'killer' => $killer,
+            'targetForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/valid-order', name: 'app_valid_order')]
+    public function validOrder(): Response
+    {
+        return $this->render('choiceUser/validOrder.html.twig');
     }
 }
---
+++ src/Entity/Target.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TargetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TargetRepository::class)]
+class Target
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $poster = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+}
